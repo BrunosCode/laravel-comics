b0e6f52f0fdff873be9afe60ac07198577f58d26
brunEdo <edobruno.m@gmail.com>
2021-11-07 22:35:01 +0100

    Header partials

---
+++ resources/views/components/Header.blade.php
@@ -0,0 +1,18 @@
+<header class="c-header">
+  <div class="l-container l-row l-row--spacebetween">
+      <a href="#">
+          <img  class="c-header__logo" src="{{ asset("images/dc-logo.png")}}" alt="DC Logo">
+      </a>
+      <nav class="c-header__nav">
+          <ul class="l-row h-listReset">
+              @foreach (config('headerNav') as $link)
+              <li class="h-align--stretch">
+                  <a href="{{ $link["url"] }}" class="c-header__link c-link {{$link["current"] ? "active" : ""}}">
+                      {{ $link["text"] }}
+                  </a>
+              </li>
+              @endforeach
+          </ul>
+      </nav>
+  </div>
+</header>
